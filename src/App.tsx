8391e3e5e3fb363d2ba76190f5e4401ace5c0233
lasconi18 <nicolasthalesmariano@gmail.com>
2025-02-28 19:59:32 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from './components/Card';
+
+const App: React.FC = () => {
+  return (
+    <Card title={''} description={''}/>
+  );
+};
+
+export default App;
